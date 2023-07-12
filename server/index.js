f6a5dc60ee5173af2f765a712aba4afa311776e9
unmukt-singh <unmukts4@gmail.com>
2023-07-12 09:04:26 +0530

    Changed vercel.json

--- server/index.js
+++ server/index.js
@@ -67,3 +67,16 @@ mongoose
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
